297054cdd2dbc52640feaad2e8db249c83eb3d4c
Byting Bulldogs <bbprogrammers3539@gmail.com>
2017-02-18 13:53:15 -0500

    Clean Shooter more
    Kenny

--- src/org/usfirst/frc/team3539/robot/subsystems/Shooter.java
+++ src/org/usfirst/frc/team3539/robot/subsystems/Shooter.java
@@ -1,10 +1,8 @@
 package org.usfirst.frc.team3539.robot.subsystems;
 import org.usfirst.frc.team3539.robot.RobotMap;
-import org.usfirst.frc.team3539.robot.Robot;
 
 import com.ctre.CANTalon;
 
-import edu.wpi.first.wpilibj.Encoder;
 import edu.wpi.first.wpilibj.smartdashboard.SmartDashboard;
 
 public class Shooter extends BulldogSystem
@@ -13,21 +11,16 @@ public class Shooter extends BulldogSystem
 	private CANTalon shooterTwoMotor;
 	private CANTalon shooterServo;
 	private CANTalon agitatorTalon;
-	
-	//private Encoder servoEncoder;
-	
+
 	public Shooter()
 	{
 		super("Shooter");
-		//shooterTwoMotor.setFeedbackDevice(CANTalon.FeedbackDevice.CtreMagEncoder_Absolute);
+		
     	//shooterTwoMotor.setStatusFrameRateMs(CANTalon.StatusFrameRate.QuadEncoder, 10);
     	//shooterTwoMotor.setEncPosition(0);
 		
-		//shooterOneMotor.setFeedbackDevice(CANTalon.FeedbackDevice.CtreMagEncoder_Absolute);
     	//shooterOneMotor.setStatusFrameRateMs(CANTalon.StatusFrameRate.QuadEncoder, 10);
     	//shooterOneMotor.setEncPosition(0);
-    	
-    	
 
 		shooterOneMotor = new CANTalon(RobotMap.shooterOneMotorTalon);
 		shooterTwoMotor = new CANTalon(RobotMap.shooterTwoMotorTalon);
@@ -44,12 +37,10 @@ public class Shooter extends BulldogSystem
 
 	public void setMotorPower(double power)
 	{
-		
 		shooterOneMotor.set(power);
 		shooterTwoMotor.set(power);
 		System.out.println(shooterOneMotor.getPulseWidthVelocity());
 		System.out.println(shooterTwoMotor.getPulseWidthVelocity());
-
 	}
 	
 	@SuppressWarnings("deprecation")
