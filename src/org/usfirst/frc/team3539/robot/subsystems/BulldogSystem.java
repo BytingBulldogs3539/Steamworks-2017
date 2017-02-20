0d8d5bdf59d42755b5f24fb78727ac82b03237cb
BBProgrammers3539 <bbprogrammers3539@gmail.com>
2017-02-20 17:27:08 -0500

    Kevin
    Cleaned up the commenting and formatting of the class

--- src/org/usfirst/frc/team3539/robot/subsystems/BulldogSystem.java
+++ src/org/usfirst/frc/team3539/robot/subsystems/BulldogSystem.java
@@ -3,20 +3,39 @@ package org.usfirst.frc.team3539.robot.subsystems;
 import edu.wpi.first.wpilibj.command.Subsystem;
 
 /**
- *
+ * Base class for all team 3539 subsystems
+ * 
+ * @author Programming team
+ * 
+ * @version 1.0
  */
 public abstract class BulldogSystem extends Subsystem
 {
+	/**
+	 * Constructor - passes the System name up the chain
+	 * 
+	 * @param name - Name of the subsystem being constructor
+	 */
 	public BulldogSystem(String name)
 	{
 		super(name);
 	}
-
-	public abstract void Update();
-
-	public abstract void SmartInit();
-
+	
+	/**
+	 * Initialize the default command - empty initialization
+	 */
 	public void initDefaultCommand()
 	{
 	}
+	
+	/**
+	 * Update the smart dash - must be implemented by the subclass
+	 */
+	public abstract void Update();
+	
+	/**
+	 * Initialize the smart dash - must be implemented by the subclass
+	 */
+	public abstract void SmartInit();
+	
 }
