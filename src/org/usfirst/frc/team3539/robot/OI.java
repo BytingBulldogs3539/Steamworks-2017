a1ed9507495c76661b1c2a00cb5c5ff99c7a3a20
Byting Bulldogs <bbprogrammers3539@gmail.com>
2017-02-23 20:03:17 -0500

    For some reason the last commit didn't include the "If"'s???

--- src/org/usfirst/frc/team3539/robot/OI.java
+++ src/org/usfirst/frc/team3539/robot/OI.java
@@ -65,14 +65,17 @@ public class OI
 	private boolean latch = false;
 	public boolean isButtonPressed() // I'm sorry for this method. Hopefully it at least works
 	{
+		if(!onebuttona.get())
 		{
 			latch = false;
 			return false;
 		}
+		if(!latch && onebuttona.get())
 		{
 			latch = true;
 			return true;
 		}
+		if(latch && onebuttona.get())
 		{
 			latch = true;
 			return false;
