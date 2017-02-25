1f8d5b4cde8fb62791e90cdf23ceaa7b74aba2ec
Byting Bulldogs <bbprogrammers3539@gmail.com>
2017-02-25 12:50:34 -0500

    Add ForwardGearPlaceGroup and TurnLeftGearPlaceGroup
    Filip

---
+++ src/org/usfirst/frc/team3539/robot/auton/TurnLeftGearPlaceGroup.java
@@ -0,0 +1,21 @@
+package org.usfirst.frc.team3539.robot.auton;
+
+import org.usfirst.frc.team3539.robot.commands.AutonDrive;
+import org.usfirst.frc.team3539.robot.commands.AutonToggleGearCommand;
+import org.usfirst.frc.team3539.robot.commands.AutonTurn;
+
+import edu.wpi.first.wpilibj.command.CommandGroup;
+
+/**
+ *
+ */
+public class TurnLeftGearPlaceGroup extends CommandGroup
+{
+	public TurnLeftGearPlaceGroup()
+	{
+		addSequential(new AutonDrive(5));
+		addSequential(new AutonTurn(-5));
+		addSequential(new AutonDrive(5));
+		addSequential(new AutonToggleGearCommand());
+	}
+}
