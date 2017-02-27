71458bfd85992ed3a1bcd3d32532e49193d335af
BBProgrammers3539 <bbprogrammers3539@gmail.com>
2017-02-27 18:52:25 -0500

    Add shooter auton groups
    Devon - lol

--- src/org/usfirst/frc/team3539/robot/auton/Blue1.java
+++ src/org/usfirst/frc/team3539/robot/auton/Blue1.java
@@ -9,43 +9,6 @@ public class Blue1 extends CommandGroup {
 
     public Blue1(int type)
     {
-        // Add Commands here:
-        // e.g. addSequential(new Command1());
-        //      addSequential(new Command2());
-        // these will run in order.
-
-        // To run multiple commands at the same time,
-        // use addParallel()
-        // e.g. addParallel(new Command1());
-        //      addSequential(new Command2());
-        // Command1 and Command2 will run in parallel.
-
-        // A command group will require all of the subsystems that each member
-        // would require.
-        // e.g. if Command1 requires chassis, and Command2 requires arm,
-        // a CommandGroup containing them would require both the chassis and the
-        // arm.
-    	
-//    	if(action.toLowerCase().contains("cross")) // pseudo wrestling
-//    	{
-//    		//addSequential(new squidward());
-//    	}
-//    	if(action.toLowerCase().contains("gear"))
-//    	{
-//    		//addSequential(new turnt);
-//    		//addSequential(new squidward);
-//    		//addSequential(new gearvomit);
-//    	}
-//    	if(action.toLowerCase().contains("shoot"))
-//    	{
-//    		//addSequential(new back);
-//    		//addSequential(new turnt);
-//    		//addSequential(new squidward);
-//    		//addSequential(new turnt);
-//    		//addSequential(new squidward);
-//    		//addSequential(new turnt);
-//    		//addSequential(new shooty);
-//    	}
-    	
+       addSequential(new TurnRightGearPlaceGroup());
     }
 }
