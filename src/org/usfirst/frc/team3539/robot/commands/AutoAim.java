0e54f6da35cbab3b9467911e28d8ce7b01998374
Byting Bulldogs <bbprogrammers3539@gmail.com>
2017-02-28 21:07:31 -0500

    AUTOAIM
    Cameron

--- src/org/usfirst/frc/team3539/robot/commands/AutoAim.java
+++ src/org/usfirst/frc/team3539/robot/commands/AutoAim.java
@@ -58,6 +58,6 @@ public class AutoAim extends PIDCommand
 	@Override
 	protected void usePIDOutput(double output)
 	{
-		Robot.driveTrain.turnLinear(output);
+		Robot.driveTrain.turnLinear(-output);
 	}
 }
