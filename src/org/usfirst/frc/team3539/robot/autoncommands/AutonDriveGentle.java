b23b3d9929bb5ac0ed6ec5f1842440773512a2b0
Byting Bulldogs <bbprogrammers3539@gmail.com>
2017-03-11 12:33:05 -0500

    Add AutonDriveGentle
    Kenny

---
+++ src/org/usfirst/frc/team3539/robot/autoncommands/AutonDriveGentle.java
@@ -0,0 +1,77 @@
+package org.usfirst.frc.team3539.robot.autoncommands;
+
+import org.usfirst.frc.team3539.robot.Robot;
+import org.usfirst.frc.team3539.robot.RobotMap;
+
+import edu.wpi.first.wpilibj.command.PIDCommand;
+
+/**
+ *
+ */
+public class AutonDriveGentle extends PIDCommand
+{
+	private double myTicks;
+	private double speedLimit;
+	
+	public AutonDriveGentle(double inches, double speedLimit)
+	{
+		super("test", RobotMap.drivePea, RobotMap.driveEye, RobotMap.driveDee);
+		Robot.driveTrain.zeroEncoders();
+		myTicks = Robot.driveTrain.inchToEnc(inches);
+		requires(Robot.driveTrain);
+		
+		this.speedLimit = speedLimit;
+		
+		this.getPIDController().setOutputRange(-.7, .7);
+		setSetpoint(myTicks);
+	}
+
+	protected void initialize()
+	{
+		this.getPIDController().setPID(RobotMap.drivePea, RobotMap.driveEye, RobotMap.driveDee);
+		Robot.driveTrain.zeroEncoders();
+		this.setSetpoint(myTicks);
+		this.getPIDController().setAbsoluteTolerance(2000);
+	}
+
+	protected void execute()
+	{
+	}
+
+	protected boolean isFinished()
+	{
+		return this.getPIDController().onTarget();
+	}
+
+	protected void end()
+	{
+		Robot.driveTrain.zeroEncoders();
+		Robot.driveTrain.stopTrain();
+	}
+
+	protected void interrupted()
+	{
+		System.out.println("AutonDrive interrupted");
+		end();
+	}
+
+	@Override
+	protected double returnPIDInput()
+	{
+		return Robot.driveTrain.getBalancedEncoderPosition();
+	}
+
+	@Override
+	protected void usePIDOutput(double output)
+	{
+		if(output > speedLimit)
+		{
+			output = speedLimit;
+		}
+		else if(output < -speedLimit)
+		{
+			output = -speedLimit;
+		}
+		Robot.driveTrain.driveLinear(output);
+	}
+}
--- src/org/usfirst/frc/team3539/robot/autongroups/GearForwardGroup.java
+++ src/org/usfirst/frc/team3539/robot/autongroups/GearForwardGroup.java
@@ -1,9 +1,9 @@
 package org.usfirst.frc.team3539.robot.autongroups;
 
-
 import org.usfirst.frc.team3539.robot.RobotMap;
 import org.usfirst.frc.team3539.robot.autoncommands.AutoWait;
 import org.usfirst.frc.team3539.robot.autoncommands.AutonDrive;
+import org.usfirst.frc.team3539.robot.autoncommands.AutonDriveGentle;
 import org.usfirst.frc.team3539.robot.autoncommands.AutonGearClose;
 import org.usfirst.frc.team3539.robot.autoncommands.AutonGearOpen;
 import org.usfirst.frc.team3539.robot.autoncommands.AutonHoodClose;
@@ -20,34 +20,18 @@ public class GearForwardGroup extends CommandGroup
 {
 	public GearForwardGroup()
 	{
-	    // Log the various steps of this auton
-	    BulldogLogger.getInstance().logInfo("Starting GearForwardGroup");
-	    
-	    BulldogLogger.getInstance().logInfo("  Starting 1st Drive Forward");
-		addSequential(new AutonDrive(50)); // orig. speedcap = 1
-		BulldogLogger.getInstance().logInfo("    Ending 1st Drive Forward");
-		
-		BulldogLogger.getInstance().logInfo("  Starting 2nd Drive forward");
-		addSequential(new AutonDrive(20)); // orig. speedcap = .4
-		BulldogLogger.getInstance().logInfo("    Ending 2nd Drive forward");
-		
-		// Add a step to wait for a fraction of a second before I open the gear
-		BulldogSleeper.sleep(250);
-		
-		// Don't care to log the GearOpen and HoodOpen steps
-		addSequential(new AutonGearOpen());
-		addSequential(new AutonHoodOpen());
-		
-		BulldogSleeper.sleep(2000)
-;
-		BulldogLogger.getInstance().logInfo("  Starting 1st drive backward");
-		addSequential(new AutonDrive(-20)); // orig. speedcap = 1
-		BulldogLogger.getInstance().logInfo("    Ending 1st drive backward");
-		
-		// Don't care to log the GearClose and HoodClose steps
-		addSequential(new AutonGearClose());
-		addSequential(new AutonHoodClose());
 
-        BulldogLogger.getInstance().logInfo("  Ending GearForwardGroup");	
+		addSequential(new AutonDrive(50)); // orig. speedcap = 1
+
+		addSequential(new AutonDriveGentle(20, .4)); // orig. speedcap = .4
+
+		
+		addSequential(new AutonGearOpen());
+
+		addSequential(new AutoWait(1));
+		
+		addSequential(new AutonDriveGentle(-20, .5)); // orig. speedcap = 1
+
+		addSequential(new AutonGearClose());
 	}
 }
