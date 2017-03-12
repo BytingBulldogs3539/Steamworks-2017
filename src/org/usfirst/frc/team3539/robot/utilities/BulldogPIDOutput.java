d68c70a47563359202695347c62b6e2d4c4ed069
BBProgrammers3539 <bbprogrammers3539@gmail.com>
2017-03-12 12:48:58 -0400

    Add for BulldogPID Output class

---
+++ src/org/usfirst/frc/team3539/robot/utilities/BulldogPIDOutput.java
@@ -0,0 +1,23 @@
+package org.usfirst.frc.team3539.robot.utilities;
+
+import edu.wpi.first.wpilibj.PIDOutput;
+
+public class BulldogPIDOutput implements PIDOutput
+{
+	private double pidValue = 0.0;
+	
+	public void pidWrite(double output)
+	{
+		pidValue = output;
+		
+	}
+	public void Reset()
+	{
+		pidValue = 0;
+	}
+	public double Get()
+	{
+		return pidValue;
+	}
+	
+}
