e3d30994aaeebabcef821b9d32924fca3461088d
Byting Bulldogs <bbprogrammers3539@gmail.com>
2017-04-26 16:02:31 -0400

    adjust autons

--- src/org/usfirst/frc/team3539/robot/autongroups/OperationThunderDogBlue.java
+++ src/org/usfirst/frc/team3539/robot/autongroups/OperationThunderDogBlue.java
@@ -27,7 +27,7 @@ public class OperationThunderDogBlue extends CommandGroup
 
 		addParallel(new HoodReset(3));
 		
-		addSequential(new SuperDriveAuton(110, .6, 50, false, 4, RobotMap.whiteLineDistance-45));
+		addSequential(new SuperDriveAuton(115, .6, 50, false, 4, RobotMap.whiteLineDistance-40));
 		addParallel(new AutonGearOpen());
 		
 		addSequential(new SuperDriveAuton(-125, .5, 50, false, 2, 0));
@@ -43,11 +43,11 @@ public class OperationThunderDogBlue extends CommandGroup
 		//addSequential(new SuperDriveAuton(-120, -.40, 30, false, 5, 0));
 
 //
-		addSequential(new AutonDrive(-60, 4,true));
+		addSequential(new AutonDrive(-60, 2.5,true));
 
 		addSequential(new SuperDriveAuton(100, -.7, 40, false, 2,20));
 
-		addSequential(new AutoWait(1));// wait to pull good distance averages
+		addSequential(new AutoWait(.25));// wait to pull good distance averages
 
 		addSequential(new AutonTurn(0, 2));// vision track turn
 
