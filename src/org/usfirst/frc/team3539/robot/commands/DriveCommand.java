79603311076bc59d5d2c40843b7ec21eb339f35f
IVan <bbprogrammers3539@gmail.com>
2017-10-11 20:35:47 -0400

    Revert "Merge branch 'master' of https://github.com/BytingBulldogs3539/Steamworks-2017"
    This reverts commit e3d980b3f9af39d8d5565aef1a485e60baf885d8, reversing
    changes made to 839acb1c9f393a744cc8de7ae2953782306cb472.

--- src/org/usfirst/frc/team3539/robot/commands/DriveCommand.java
+++ src/org/usfirst/frc/team3539/robot/commands/DriveCommand.java
@@ -39,11 +39,11 @@ public class DriveCommand extends BulldogCommand
 
 		if (Robot.oi.onebumperr.get())
 		{
-			Robot.driveTrain.driveArcade(-Robot.oi.controller1.getRawAxis(RobotMap.Y_AxisL), Robot.oi.controller1.getRawAxis(RobotMap.X_AxisR));
+			Robot.driveTrain.driveArcade(-Robot.oi.controller1.getRawAxis(RobotMap.LEFT_TRIGGER), Robot.oi.controller1.getRawAxis(RobotMap.RIGHT_TRIGGER));
 		}
 		else
 		{
-			Robot.driveTrain.driveArcade(Robot.oi.controller1.getRawAxis(RobotMap.Y_AxisL), Robot.oi.controller1.getRawAxis(RobotMap.X_AxisR));
+			Robot.driveTrain.driveArcade(Robot.oi.controller1.getRawAxis(RobotMap.LEFT_TRIGGER), Robot.oi.controller1.getRawAxis(RobotMap.RIGHT_TRIGGER));
 		}
 	}
 
